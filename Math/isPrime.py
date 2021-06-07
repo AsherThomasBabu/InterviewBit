74713c80ab01bd5f7e019cd90dba17bcde5bc5a6
AsherThomasBabu <ashertb2000@gmail.com>
2021-06-07 12:44:31 +0530

    added isPrime

---
+++ Math/isPrime.py
@@ -0,0 +1,13 @@
+# Given a number N, verify if N is prime or not.
+# Return 1 if N is prime, else return 0.
+
+class Solution:
+    # @param A : integer
+    # @return an integer
+    def isPrime(self, A):
+        if A <= 1:
+            return 0
+        for i in range(2,int(A**0.5)+1):
+            if(A%i==0):
+                return 0
+        return 1
